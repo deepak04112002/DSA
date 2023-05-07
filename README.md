4ef218a2f03cf8d6dbcf005dfcd79e97ca85ea18
Your Name <you@example.com>
2023-05-07 09:42:03 +0530

    Selection Sort

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 1.Patterns</br>
 2.STL</br>
 3.Basic Maths</br>
-4.Recursion
+4.Recursion</br>
+4.Sorting
---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+class Solution
+{
+    public:
+    void selectionSort(int arr[], int n)
+    {
+       //code here
+       for(int i=0;i<=n-2;i++){
+           int mini=i;
+           for(int j=i;j<=n-1;j++){
+               if(arr[j]<arr[mini])
+               mini=j;
+           }
+           int temp=arr[mini];
+           arr[mini]=arr[i];
+           arr[i]=temp;
+       }
+    }
+};
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  
+    ob.selectionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
