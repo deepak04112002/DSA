e215ef20405b63436db8976e040ceed7c2fdc432
Your Name <you@example.com>
2023-05-08 22:59:57 +0530

    BubbleSort useing Recursion

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -56,3 +56,59 @@ int main()
 }
 //Time Complexity is O(n^2) for worst,average case
 //best case O(N);
+
+
+//Usig Recursion
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+class Solution
+{
+    public:
+    void bubbleSort(int arr[], int n)
+    {
+        if(n==1)
+        return;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                int temp=arr[i+1];
+                arr[i+1]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        bubbleSort(arr,n-1);
+    }
+};
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main()
+{
+    int arr[1000],n,T,i;
+  
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      
+    Solution ob;  
+      
+    ob.bubbleSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;;
+}
