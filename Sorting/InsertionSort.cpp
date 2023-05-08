df5ee680d4ac164b26a7859bdbf0e7e280e2ba82
Your Name <you@example.com>
2023-05-08 23:21:37 +0530

    insertionSorting using Recursion

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -43,4 +43,54 @@ int main()
     return 0;
 }
 //Time Complexity O(N^2) for worst and average case.
-//O(N)for best case.
+//O(N)for best case.//Time Complexity is same for both but iterative take less memory space compared to recurssive one.
+
+//Recursion
+#include <stdio.h>
+#include <math.h>
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+class Solution
+{
+
+     public:
+    void insertionSort(int arr[], int n)
+    {
+        if(n<=1)
+        return;
+        for(int i=0;i<=n-1;i++){
+            int j=i;
+            if((arr[j-1]>arr[j])&&j>0){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+        insertionSort(arr,n-1);
+    }
+};
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
