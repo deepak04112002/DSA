71c4ab61231457b20fa5687e73bb05114b128c5e
Your Name <you@example.com>
2023-05-09 16:02:42 +0530

    CodeStudio

--- Array/Easy/2ndMaxElementinArray.cpp
+++ Array/Easy/2ndMaxElementinArray.cpp
@@ -33,3 +33,38 @@ int main() {
     }
     return 0;
 }
+//Codestudio
+//find 2nd largest and 2nd smallest element
+int secondLargest(vector<int> &a,int n){
+   int largest=a[0];
+   int slarge=-1;
+   for(int i=1;i<n;i++){
+       if(a[i]>largest){
+           slarge=largest;
+           largest=a[i];
+       }
+       else if(a[i]<largest&&a[i]>slarge){
+           slarge=a[i];
+       }
+   }
+   return slarge;
+}
+int secondSmallest(vector<int> &a,int n){
+        int smallest=a[0];
+        int ssmall=INT_MAX;
+        for(int i=1;i<n;i++){
+            if(a[i]<smallest){
+                ssmall=smallest;
+                smallest=a[i];
+            }
+            else if(a[i]>smallest&&a[i]<ssmall){
+                ssmall=a[i];
+            }
+        }
+        return ssmall;
+}
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    int slargest=secondLargest(a,n);
+    int ssmallest=secondSmallest(a,n);
+    return {slargest,ssmallest};
+}
