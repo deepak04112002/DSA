dd3b2f65a8798c0caec81e9d617d1ba7337de013
Your Name <you@example.com>
2023-05-09 15:45:40 +0530

    Array

---
+++ Array/Easy/2ndMaxElementinArray.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+public:	
+	int print2largest(int arr[], int n) {
+	    int largest=arr[0];
+	    int slargest=-1;
+	    for(int i=1;i<n;i++){
+	        if(arr[i]>largest){
+	            slargest=largest;
+	            largest=arr[i];
+	        }
+	        else if(arr[i]<largest&&arr[i]>slargest){
+	            slargest=arr[i];
+	        }
+	    }
+	    return slargest;
+	}
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
