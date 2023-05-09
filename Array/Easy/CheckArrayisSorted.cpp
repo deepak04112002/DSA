71a4b1d06eb5819b8b4d2e436c2d06c32fdbb93e
Your Name <you@example.com>
2023-05-09 17:18:28 +0530

    Leetcode

--- Array/Easy/CheckArrayisSorted.cpp
+++ Array/Easy/CheckArrayisSorted.cpp
@@ -27,20 +27,3 @@ int main() {
     }
     return 0;
 }
-//Leetcode
-//checkArraySorted and rotated once
-class Solution {
-public:
-    bool check(vector<int>& nums) {
-        int count=0;
-        int n=nums.size();
-        for(int i=1;i<n;i++){
-            if(nums[i-1]>nums[i])
-            count++;
-            }
-            if(nums[n-1]>nums[0])
-            count++;
-        
-        return count<=1;
-    }
-};
