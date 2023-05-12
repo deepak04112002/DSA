fe068cfb705dc8c7f6435fc45a5022417bd08fe9
Your Name <you@example.com>
2023-05-12 23:34:21 +0530

    Codestudio part

--- Array/2sum.cpp
+++ Array/2sum.cpp
@@ -14,3 +14,19 @@ public:
         return {-1,-1};
     }
 };
+//if only we want to check the sum//optimal solution is
+//O(n)+)(nlogn)//time complexity
+//O(1)//if it sorted else O(n) if not sorted is cosidered
+string read(int n, vector<int> book, int target)
+{
+    sort(book.begin(),book.end());
+    int left=0;
+    int right=n-1;
+    while(left<right){
+        int sum=book[left]+book[right];
+        if(sum==target) return "YES";
+        else if(sum>target)right--;
+        else if(sum<target)left++;
+    }
+    return "NO";
+}
