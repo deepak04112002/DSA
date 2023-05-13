4325594ac7fb3ecc73c97ecd2c0ee450d00b9999
Your Name <you@example.com>
2023-05-13 22:01:25 +0530

    gfg

--- Array/2sum.cpp
+++ Array/2sum.cpp
@@ -30,3 +30,58 @@ string read(int n, vector<int> book, int target)
     }
     return "NO";
 }
+//Find all pairs with a given sum
+#include <bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    static bool cmp(pair<int,int>a,pair<int,int> b){
+        return a.first<b.first;
+    }
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    { 
+        vector<pair<int,int>> ans;
+        unordered_set<int> s;
+        for(int i=0;i<N;i++){
+            s.insert(A[i]);
+        }
+        for(int i=0;i<M;i++){
+            if(s.find(X-B[i])!=s.end())
+            ans.push_back({X-B[i],B[i]});
+        }
+        sort(ans.begin(),ans.end(),cmp);
+        return ans;
+        
+    }
+};
+int main() {
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    int N, M, X;
+	    cin >> N >> M >> X;
+        int A[N], B[M];
+        
+	    for(int i = 0;i<N;i++)
+	        cin >> A[i];
+	    for(int i = 0;i<M;i++)
+	        cin >> B[i];
+	        
+	   Solution ob;
+	   vector<pair<int,int>> vp = ob.allPairs(A, B, N, M, X);
+	   int sz = vp.size();
+        if(sz==0)
+        cout<<-1<<endl;
+        else{
+            for(int i=0;i<sz;i++){
+                if(i==0)
+                cout<<vp[i].first<<" "<<vp[i].second;
+                else
+                cout<<", "<<vp[i].first<<" "<<vp[i].second;
+            }
+            cout<<endl;
+        }
+	}
+	return 0;
+}
