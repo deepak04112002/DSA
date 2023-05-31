84a746b60e18b0b1440475a65a6fa23129f1c57c
Your Name <you@example.com>
2023-06-01 00:36:15 +0530

    Reverse LinkedList using iteration and Recursion

--- LinkedList/Reversealinkedlist.cpp
+++ LinkedList/Reversealinkedlist.cpp
@@ -13,3 +13,34 @@ public:
         return dump;
     }
 };
+//iterative
+lass Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==NULL || head->next==NULL)
+        return head;
+        ListNode*Prev=NULL;
+        ListNode*curr=head;
+        ListNode*Next=head->next;
+        while(Next){
+            curr->next=Prev;
+            Prev=curr;
+            curr=Next;
+            Next=Next->next;
+        }
+        curr->next=Prev;
+        return curr;
+    }
+};
+//recursive
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next)
+        return head;
+        auto ans=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return ans;
+    }
+};
